5efdf6e8e8c27c35cf3e412c7503682a719da238
Agnel Joseph <Agnel.Joseph1@ibm.com>
2022-02-09 15:15:29 +0400

    corrected node server port

--- exercise-1/server.js
+++ exercise-1/server.js
@@ -5,6 +5,6 @@ app.use(express.static('public'))
 
 app.use(express.static(__dirname))
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Listening: http://localhost:${port}`))
